5e7b349a011f740c9892b1780e91b45777483fe9
Thomas Bakker <thomasb.work@gmail.com>
2019-07-17 16:46:40 +0200

    Add unit test for user model

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+use App\Task;
+use Illuminate\Database\Eloquent\Collection;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    /** @test */
+    public function it_has_many_tasks()
+    {
+        $user = factory(User::class)->create();
+        factory(Task::class)->create(['user_id'=> $user->id]);
+
+        $this->assertInstanceOf(Task::class, $user->tasks->first());
+        $this->assertInstanceOf(Collection::class, $user->tasks);
+    }
+}
