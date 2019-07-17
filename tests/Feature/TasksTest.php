30a136d18797a63acb20a58a8447b260fe38bf45
Thomas Bakker <thomasb.work@gmail.com>
2019-07-17 16:27:55 +0200

    Rename test class

--- tests/Feature/TasksTest.php
+++ tests/Feature/TasksTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class UserTasksTest extends TestCase
+class TasksTest extends TestCase
 {
     use RefreshDatabase;
 
